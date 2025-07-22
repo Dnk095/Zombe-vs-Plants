d43e1fa4f1bcb1f0db1b8fff7bd78dd2aae4e13b
Dnk095 <darcnikill@gmail.com>
2025-07-22 09:35:55 +0300

    Create InputReader.cs

---
+++ Assets/Scripts/Camera/InputReader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+internal class InputReader: MonoBehaviour
+{
+    private const string Horizontal = "Horizontal";
+    private const string Vertical = "Vertical";
+
+   // private int _mouseButtonSelect = 0;
+   // private int _mouseButtonDeselect = 1;
+
+    public float VerticalDirection { get; private set; }
+    public float HorizontalDirection { get; private set; }
+
+    //public event Action Selecting;
+   // public event Action Deselecting;
+
+    private void Update()
+    {
+        HorizontalDirection = Input.GetAxis(Horizontal);
+        VerticalDirection = Input.GetAxis(Vertical);
+
+        //if (Input.GetMouseButtonDown(_mouseButtonSelect))
+           // Selecting?.Invoke();
+       // else if (Input.GetMouseButtonDown(_mouseButtonDeselect))
+           // Deselecting?.Invoke();
+    }
+}
